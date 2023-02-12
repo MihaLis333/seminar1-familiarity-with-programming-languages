b69fcd55848d65fd5a7fba66de9ea53b288cdae2
MihaLis333 <m190333@ya.ru>
2023-02-12 10:53:50 +0300

    add two tasks finding the square of a number

--- seminar1/Program.cs
+++ seminar1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+double a = 9;
+double b = 3;
+
+if (a * a == b)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("No");
+} */
+
+
+/* Console.WriteLine("Введите число");
+double a = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Квадратa числа a = {a * a}"); */
+
